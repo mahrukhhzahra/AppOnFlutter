783b4396741cebe6f0604eb36c98e1974c147773
Mahrukh <mahrukhrizvi2@gmail.com>
2022-12-03 23:22:30 +0500

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:take2/Home_Page.dart';
 
 void main() {
-
   runApp(MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child : Center(
-          child: Container(
-            child: Text("Welcome Mahrukh")
-          ),
-        ),
-      ),
-    );
 
-        
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
